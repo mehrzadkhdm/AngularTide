7a525b142482d9b1b5cb5aa8dce3d815f114ce79
Mehrzad <mehrzadkhdm@gmail.com>
2022-08-04 13:32:49 -0500

    Moved fetch data  to home

--- Tide/ClientApp/src/app/home/home.component.ts
+++ Tide/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+    selector: 'app-home',
+    templateUrl: './home.component.html',
 })
+
 export class HomeComponent {
+    public forecasts: WeatherForecast[];
+    public ports: PortName[];
+
+    constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+        http.get<PortName[]>(baseUrl + 'port').subscribe(result => {
+            this.ports = result;
+        }, error => console.error(error));
+
+    }
 }
+
+interface WeatherForecast {
+    date: string;
+    temperatureC: number;
+    temperatureF: number;
+    summary: string;
+}
+
+interface PortName {
+    name: string;
+}
+
